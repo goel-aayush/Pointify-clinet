dba9edf5d8c7d2e939a3270c9f35547e89ae743c
aayush <mgoel1296@gmail.com>
2025-07-29 16:02:26 +0530

    new

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-const baseURL =
-  import.meta.env.MODE === "development"
-    ? "https://pointify-hki2.onrender.com/api/user"
-    : "/api/user";
+const baseURL = "https://pointify-hki2.onrender.com/api/user";
+// import.meta.env.MODE === "development"
+//   ? "https://pointify-hki2.onrender.com/api/user"
+//   : "/api/user";
 
 export const axiosInstance = axios.create({
   baseURL,
